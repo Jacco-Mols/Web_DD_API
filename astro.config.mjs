66d9144c8be512670c301cc0c60ed7e1c38ec710
Jacco12 <molsjacco@gmail.com>
2026-04-22 11:21:05 +0200

    Enable astro servermodus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-    adapter: node({mode: 'standalone'}),
+  adapter: node({
+    mode: 'standalone',
+  }),
+  output: 'server'
 });
